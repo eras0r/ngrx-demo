87f70376954a5c3b5fef35fdf9159242547b1303
eras0r <er4s0r@gmail.com>
2019-12-03 16:12:53 +0100

    - rename app from ngrx-demo2 to ngrx-demo - change default collections to ngrx schematics

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngrx-demo2'`, () => {
+  it(`should have as title 'ngrx-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngrx-demo2');
+    expect(app.title).toEqual('ngrx-demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngrx-demo2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ngrx-demo app is running!');
   });
 });
