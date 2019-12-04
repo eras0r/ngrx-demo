8aae9ea74b852272312dfbeffff39873c9f7fab5
eras0r <er4s0r@gmail.com>
2019-12-04 09:34:08 +0100

    setup core effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import {StoreModule} from '@ngrx/store';
 import {metaReducers, reducers} from './core/core.state';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {environment} from '../environments/environment';
+import {EffectsModule} from '@ngrx/effects';
+import {coreFeatureEffects} from './core/core.effects';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import {environment} from '../environments/environment';
         strictActionImmutability: true,
       }
     }),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot(coreFeatureEffects)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.effects.ts
@@ -0,0 +1,6 @@
+// TODO import core effects here
+// import {UserEffects} from './user.effects';
+
+export const coreFeatureEffects = [
+  // UserEffects // TODO use core effects here
+];
