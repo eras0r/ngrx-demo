8cf89b687a046e511f456033b8688afd9f6e1b65
eras0r <er4s0r@gmail.com>
2019-12-10 16:55:05 +0100

    suppress lint issues

--- src/app/todos/todos.effects.spec.ts
+++ src/app/todos/todos.effects.spec.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { TodosEffects } from './todos.effects';
 
 describe('TodosEffects', () => {
+  // tslint:disable-next-line:prefer-const
   let actions$: Observable<any>;
   let effects: TodosEffects;
 
